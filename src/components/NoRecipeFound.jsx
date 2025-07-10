a8899217b3496027d6a865a2e64507784406d8f1
Karl <karlnino.bautista@tup.edu.ph>
2025-07-11 00:58:48 +0800

    erased the auth system

--- src/components/NoRecipeFound.jsx
+++ src/components/NoRecipeFound.jsx
@@ -1,14 +1,20 @@
 
 import "../css/NoRecipeFound.css"
 import NoRecipe from "../assets/no-recipe.svg"
+import InputForm from "./InputForm";
+import ListLetters from "./ListLetters";
+import Flags from "./Flags";
 function NoRecipeFound({recipe}){
 
     return(
         <div className="no-recipe-found-container">
+            <InputForm />
             <p>{`Sorry, recipe "${recipe}" is not found`}</p>
             <div className="no-recipe-found-img-container">
                 <img src={NoRecipe} alt="no recipe img" />
             </div>
+            <ListLetters />
+            <Flags />
           
         </div>
     )
