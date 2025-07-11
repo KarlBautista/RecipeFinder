ab7b5263e4a8c87c1e17cab4ee6d9a99e4026cff
Karl <karlnino.bautista@tup.edu.ph>
2025-07-11 18:27:32 +0800

    put my socials in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,20 @@
 import "../css/Footer.css";
+import Facebook from "../assets/facebook.png"
+import LinkedIn from "../assets/linkedin.png"
+import Github from "../assets/github-sign.png"
 
 function Footer() {
     return (
         <footer className="footer">
-            <p className="footer-text">{`${new Date().getFullYear()}@Karl Bautista`}</p>
+            <div className="title">
+                  <p className="footer-text">{`${new Date().getFullYear()}@Karl Bautista`}</p>
+            </div>
+            <div className="social-links">
+                <a href="https://www.facebook.com/karl.bautista.254770" target="_blank"><img src={Facebook} alt="" /></a>
+                   <a href="https://www.linkedin.com/in/karl-bautista-86417b308/" target="_blank"><img src={LinkedIn} alt="" /></a>
+                     <a href="https://github.com/KarlBautista" target="_blank"><img src={Github} alt="" /></a> 
+            </div>
+          
         </footer>
     );
 }
