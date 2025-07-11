a00baea6530ad1bcf49e14c8b1f9fd10ed08889e
Karl <karlnino.bautista@tup.edu.ph>
2025-07-11 17:33:42 +0800

    added favorites functionality

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -2,12 +2,37 @@
 import "../css/RecipeCard.css"
 import { useSearchParams, useNavigate } from "react-router-dom";
 import { searchForRecipes } from "../services/api";
+import { useFavoritesContext } from "../context/FavoritesContext";
+import { useEffect } from "react";
 function RecipeCard({ recipe }){
+
     const navigate = useNavigate();
+    const { addToFavoritesRecipe, favoritesRecipe, isFavoriteRecipe, removeFromFavoritesRecipe } = useFavoritesContext();
+    const favorite = isFavoriteRecipe(recipe.idMeal);
+
+  
     
     const viewRecipe =  (recipe) => {
         navigate(`/recipe?q=${recipe.strMeal}`); 
     }
+
+    const addToFavorites = (recipe) => {
+        try{
+            if(favorite){
+                removeFromFavoritesRecipe(recipe.idMeal);
+            } else{
+            addToFavoritesRecipe(recipe);
+             alert(`${recipe.strMeal} is added to favorties`);
+             console.log(favoritesRecipe);
+            }
+            
+        } catch(err){
+            console.error(err)
+        }
+    }
+
+  
+    
 
     return(
         <div className="recipe-card-container">
@@ -16,10 +41,12 @@ function RecipeCard({ recipe }){
                 <img src={recipe?.strMealThumb} 
                 onClick={() => viewRecipe(recipe)} alt={`recipe image ${recipe?.idMeal}`} 
                 className="search-result-img"/>
+                <div className={favorite ? "red-heart-icon" : "heart-icon"} onClick={() => addToFavorites(recipe)}>♥</div>
             </div>
             <div className="search-result-recipe-name">
                 <p onClick={() => viewRecipe(recipe)}>{recipe?.strMeal}</p>
             </div>
+
 
         </div>
     )
---
+++ src/context/FavoritesContext.jsx
@@ -0,0 +1,61 @@
+import { useContext, createContext, useState, useEffect } from "react";
+
+const FavroitesContext  = createContext();
+
+export const useFavoritesContext = () => useContext(FavroitesContext);
+
+
+export const FavoritesProvier = ({ children }) => {
+
+    const [favoritesRecipe, setFavoritesRecipe] = useState([]);
+    
+
+    useEffect(() => {
+        try{
+            const storedFavorites = localStorage.getItem("favoritesRecipe");
+            if(storedFavorites) JSON.parse(storedFavorites);
+          
+        } catch(err){
+            console.error(err);
+        }
+   }, []) 
+
+   
+    useEffect(() => {
+        try{
+            localStorage.setItem("favoritesRecipe", JSON.stringify(favoritesRecipe))
+        } catch(err){
+            console.error(err);
+        }
+    }, [favoritesRecipe]);
+
+    const addToFavoritesRecipe = (recipe) => {
+        setFavoritesRecipe(f => [...f, recipe]);
+    }
+
+    const isFavoriteRecipe = (favoriteId) => {
+        return favoritesRecipe.some(f => f.idMeal === favoriteId)
+    }
+
+    const removeFromFavoritesRecipe = (favoriteId) => {
+        setFavoritesRecipe(f => f.filter(f => f.idMeal !== favoriteId))
+    }
+
+
+    const value = {
+        addToFavoritesRecipe,
+        favoritesRecipe,
+        isFavoriteRecipe,
+        removeFromFavoritesRecipe
+    }
+
+    
+
+    return(
+        <FavroitesContext.Provider value={value}>
+            {children}
+        </FavroitesContext.Provider>
+    )
+
+
+}
