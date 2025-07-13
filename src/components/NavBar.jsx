b798f71d70008291db4ce550caa1b998a2f52b32
Karl <karlnino.bautista@tup.edu.ph>
2025-07-13 21:04:11 +0800

    responsiveness in navbar and brand

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,52 @@
 import "../css/NavBar.css"
 import { Link, useLocation} from "react-router-dom";
+import Menu from "../assets/menu.png"
+import { useEffect, useState } from "react";
 
 
 
 function NavBar(){
     
     const location = useLocation();
+    
+    const [width, setWidth] = useState(window.innerWidth);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, [])
 
-
+    const burgerMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
     return(
         <div className="navbar-container">
             <div className="brand-container">
                <p><Link to="/">RecipeFinder</Link></p>
             </div>
-            <div className="links">
-                <Link to="/" 
-                className={location.pathname === "/" ? "active-link" : "" }>Home</Link>
-               <Link to="/favorites"
-               className={location.pathname === "/favorites" ? "active-link" : ""}>Favorites</Link>
+            {isMenuOpen && (
+                <div className="burger-menu-links">
+                  <Link to="/" className={location.pathname === "/" ? "active-burger-link" : ""}>Home</Link>
+                  <Link to="/favorites" className={location.pathname === "/favorites" ? "active-burger-link" : ""}>Favorites</Link>
+                </div>
+            )}
+            {
+                width < 670 ? (
+                    <div className="burger-menu">
+                        <img src={Menu} alt="" onClick={() => burgerMenu()}/>
+                    </div>
+                ) : 
+                (
+                    <div className="links">
+                    <Link to="/" 
+                    className={location.pathname === "/" ? "active-link" : "" }>Home</Link>
+                   <Link to="/favorites"
+                   className={location.pathname === "/favorites" ? "active-link" : ""}>Favorites</Link>
             </div>
+                )
+            }
+         
 
             </div>
     )
